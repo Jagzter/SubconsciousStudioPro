8302e75d65b128727594816db6b35f63ebc5d1f4
Jagzter <56849831+Jagzter@users.noreply.github.com>
2026-06-20 01:36:12 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ssp-v231-cache';
+const CACHE='ssp-v232-cache';
 const ASSETS=['./','./index.html','./css/style.css','./js/app.js','./js/audio.js','./js/player.js','./js/playlist.js','./js/statistics.js','./js/config.js','./js/storage.js','./manifest.webmanifest','./icons/logo.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
